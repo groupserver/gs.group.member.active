4c24eaaaa9925151a1c12cc16997b73e3ee940a6
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-09 11:10:21 -0700

    Adding a loading method.

--- gs/group/member/active/browser/javascript/ajax.js
+++ gs/group/member/active/browser/javascript/ajax.js
@@ -1,8 +1,15 @@
 jQuery.noConflict();
 
+function gs_group_member_active_loaded (response, status, xhr) {
+    var FADE_SPEED = 'slow', FADE_METHOD = 'swing';
+    jQuery('#gs-group-member-active-loading').fadeOut(FADE_SPEED, FADE_METHOD);
+    jQuery('#gs-group-member-active-ajax-hole').fadeIn(FADE_SPEED, FADE_METHOD);
+}
+
 jQuery(window).load(function () {
-    var baseUrl = null, url = null;
+    var baseUrl = null, url = null, hole = null;
     baseUrl = jQuery('#gs-group-member-active-list-js').attr('data-group-url');
-    url = baseUrl + '/gs-group-member-active-ajax.html';    
-    jQuery('#gs-group-member-active-ajax-hole').load(url);
+    url = baseUrl + '/gs-group-member-active-ajax.html';
+    hole = jQuery('#gs-group-member-active-ajax-hole');
+    hole.load(url, gs_group_member_active_loaded);
 });
