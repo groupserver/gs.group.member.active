e51f40feb193b06f730c9a898a084f7d13302b50
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-09 10:29:09 -0700

    Adding a copyright block.

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -34,10 +47,12 @@ setup(name='gs.group.member.active',
         'setuptools',
         'sqlalchemy',
         'gs.database',
+        'gs.group.base',
         'gs.group.home',
         'gs.group.member.viewlet',
         'zope.cachedescriptors',
         'zope.component',
+        'zope.viewlet',
     ],
     entry_points="""
     # -*- Entry points: -*-
