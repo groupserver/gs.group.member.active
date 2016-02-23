e2146a337ec3e717021917e02d8863db3111466f
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-23 08:22:29 -0800

    Adding a test for the list of active members

--- gs/group/member/active/tests/page.py
+++ gs/group/member/active/tests/page.py
@@ -13,6 +13,7 @@
 #
 ############################################################################
 from __future__ import absolute_import, unicode_literals, print_function
+from functools import partial
 from mock import (MagicMock, patch, PropertyMock)
 from unittest import TestCase
 from gs.group.member.active.page import (ActiveMembersAjax, )
@@ -50,3 +51,29 @@ class TestActiveMembersAjax(TestCase):
         a = ActiveMembersAjax(group, request)
 
         self.assertEqual([], a.userPosts)
+
+    @staticmethod
+    def create_user(factory, context, userId, anonymous):
+        retval = MagicMock()
+        retval.id = userId
+        retval.anonymous = anonymous
+        return retval
+
+    @patch.object(ActiveMembersAjax, 'userPosts', new_callable=PropertyMock)
+    @patch.object(ActiveMembersAjax, 'groupInfo', new_callable=PropertyMock)
+    @patch.object(ActiveMembersAjax, 'siteInfo', new_callable=PropertyMock)
+    @patch('gs.group.member.active.page.createObject')
+    def test_active_members(self, m_cO, m_sI, m_gI, m_uP):
+        m_gI.id = 'example_group'
+        m_sI.id = 'example'
+        m_uP.return_value = [{'user_id': u} for u in ['a', 'b', 'c', ]]
+        m_cO.side_effect = partial(self.create_user, anonymous=False)
+
+        group = MagicMock()
+        request = MagicMock()
+        a = ActiveMembersAjax(group, request)
+        r = a.activeMembers
+
+        activeMemberIds = [u.id for u in r]
+        self.assertEqual(['a', 'b', 'c', ], activeMemberIds)
+        self.assertEqual(3, m_cO.call_count)
