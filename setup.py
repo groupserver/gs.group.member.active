b787d8c5728c1cfa9880b9efc58dd7903f98f87e
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-23 08:43:18 -0800

    Adding the tests to the product metadata

--- setup.py
+++ setup.py
@@ -72,6 +72,8 @@ setup(
         'gs.group.member.base',
         'gs.group.member.viewlet',
     ],
+    test_suite="{0}.tests.test_all".format(name),
+    tests_require=['mock', ],
     entry_points="""
     # -*- Entry points: -*-
     """,)
